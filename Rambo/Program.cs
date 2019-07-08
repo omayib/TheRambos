4774734030726a805e09773696a4a8457fc1f23e
omayib <omayib@gmail.com>
2019-07-08 19:40:05 +0700

    Add project files.

---
+++ Rambo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rambo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
